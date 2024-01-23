beff6e5ef8429d4627d0aa779a6b440614b5b308
hunzilaahmed <hunziihm@gmail.com>
2024-01-23 19:13:12 +0500

    push first data

---
+++ src/Pages/Cart.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import CartItems from '../components/CardItems/CardItems'
+
+const Cart = () => {
+  return (
+    <div>
+      <CartItems/>
+    </div>
+  )
+}
+
+export default Cart
